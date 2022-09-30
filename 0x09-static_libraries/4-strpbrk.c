14571327abce27e575df9dbbe7b71193f995f8eb
TomLutherKE <tomluther133@gmail.com>
2022-09-30 06:44:23 -0700

    Task 0 complete

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for students.
+ *
+ * Return: Always 0.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+		s++;
+	while (*accept != '\0')
+		accept++;
+	return (0);
+
+}	
---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for students.
+ *
+ * Return: Always 0.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+		haystack++;
+	while (*needle != '\0')
+		needle++;
+	return (0);
+}
