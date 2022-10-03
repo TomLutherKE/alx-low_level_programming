34e4b11759fd8007b775c6e3bff30622bc79c8bf
TomLutherKE <tomluther133@gmail.com>
2022-10-02 21:26:38 -0700

    Task 2 complete

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,34 +1,52 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * str_concat - concatenates two strings
+ * str_concat - function to concatene two strings
+ * @s1: the first string
+ * @s2: the second string
  *
- * Return: Always 0.
+ * Return: a pointer to tha allocated memory Area.
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, size;
-	char *a;
+int size1 = 0;
+int size2 = 0;
+int i, j;
+char *p;
 
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-	for (j = 0; s2[j] != '\0'; j++)
-		;
-	size = i + j + 1;
-	a = malloc(sizeof(char) * size);
-	if (a == 0)
-		return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		*(a + i) = *(s1 + i);
-	}
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-		*(a + i + j) = *(s2 + j);
-	}
+if (s1 == NULL)
+	s1 = "";
+if (s2 == NULL)
+	s2 = "";
 
-	return (a);
+/* calculate the length of string 1*/
+while (*(s1 + size1))
+	size1++;
+/* calculate the length of the string 2 */
+while (*(s2 + size2))
+	size2++;
+
+/* a pointer pointed to the memory area allocation*/
+p = malloc((size1 + size2) + 1);
+
+if (p == NULL)
+	return (NULL);
+i = 0;
+/* copying the first string on the allocated memory*/
+while (i < size1)
+{
+	*(p + i) = *(s1 + i);
+			i++;
+}
+
+/* copying the second string */
+j = 0;
+while (j <= size2)
+{
+	*(p + j + size1) = *(s2 + j);
+		j++;
+}
+/* the program return a pointer pointed */
+return (p);
 }
