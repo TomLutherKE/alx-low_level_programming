50b079f1776fa0c8dfd09914f356f26f175b674a
TomLutherKE <tomluther133@gmail.com>
2022-10-24 09:36:11 -0700

    Task 3 updated

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,61 +1,56 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
-/**
- * _strlen - determines the string length
- * @str: given string
- *
- * Return: an integer length
- */
-int _strlen(const char *str)
-{
-	int length = 0;
-
-	while (str)
-		length++;
-	return (length);
-}
 
 /**
- * add_node_end - add a node at the end of list
- * @head: the head of the list
- * @str: a given string
- *
- * Return: a pointer to the new element
+ * add_node_end - Adds a new node at the end of the list
+ * @head: pointer to a pointer to a linked list
+ * @str: Pointer for a string input
+ * Return: the address of the new element, or NULL if it failed
  */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
-	list_t *tmp;
-	tmp = *head;
+	list_t *pass;
 
-	if (!str)
-		return (NULL);
-
-	/* nod memeorty allocation */
 	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 		return (NULL);
-	/* Nod creation and initialization */
-	new_node->next = NULL;
+
+	pass = *head;
+
 	new_node->str = strdup(str);
-	/* check memory allocation for strdup */
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->len = _strlen(str);
-	/* checking if there is more than 2 elements(list exist) */
+	new_node->len  = _strlen(str);
+	new_node->next = NULL;
+
 	if (*head == NULL)
 	{
 		*head = new_node;
-		return (new_node);
+		return (*head);
 	}
-	/* searcing for n-1 last element of the list where to point */
-	while (tmp->next)
+	while (pass->next != NULL)
+		pass = pass->next;
+	pass->next = new_node;
+	return (*head);
+}
+
+/**
+ * _strlen - returns the length of the string
+ *@s: string being counted
+ * Return: Always 0.
+ */
+
+int _strlen(const char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
 	{
-		tmp = tmp->next;
-	tmp->next = new_node;
+		i++;
 	}
-	return (new_node);
+	return (i);
 }
